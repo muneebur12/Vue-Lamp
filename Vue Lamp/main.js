f9ba1a9b6eb3b6a641a53e4567be162b428e20db
Muneeb Ashiq <108215727+muneebur12@users.noreply.github.com>
2023-06-11 18:38:34 +0500

    Add files via upload

---
+++ Vue Lamp/main.js
@@ -0,0 +1,9 @@
+
+var btn= document.getElementById('btn');
+var light= document.getElementById('light')
+
+function togglebtn()
+{
+    btn.classList.toggle('active');
+    light.classList.toggle('on'); 
+}
